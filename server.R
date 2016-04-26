d00dd65dca2ba82d7e0181db0d83f2d5ff4f476a
iorch <j.martinezortega@gmail.com>
2016-04-25 20:40:56 -0500

    refactoring, creating html with templtes

--- server.R
+++ server.R
@@ -12,10 +12,13 @@ library(RColorBrewer)
 library(xts)
 library(dygraphs)
 
+source("./template_html.R")
+
 ###Clinic Level Map
-states <- readOGR("data",layer = "prueba", verbose = FALSE)
+states <- readOGR(dsn="./data",layer = "prueba", verbose = T)
 states@data$Color <- ifelse(states@data$PD==1,"#00cc99","#0C5F4A")
-UM <- read.csv("data/ec_muestra_20160203.csv",stringsAsFactors = FALSE, fileEncoding = "Windows-1252") %>% subset(cl_treatmentArm!=0) 
+UM <- read.csv("./data/ec_muestra_20160203.csv",stringsAsFactors = FALSE, fileEncoding = "Windows-1252") %>% subset(cl_treatmentArm!=0) 
+MSJ <- read.csv("./data/mensajes_pd_rp.csv")
 names(UM)[21:22] <- c("lon","lat")
 HUIcon <- makeIcon(
   iconUrl = "http://104.236.151.123:3838/DashboardProspera/assets/icono-unidad.svg",
@@ -55,266 +58,101 @@ TimeData$Acc <- NULL
 
 
 
-
-
-
 ########################################################################################################
 shinyServer(function(input, output) {
   
   output$mapaprueba <- renderLeaflet({
     if(input$nivel=="Unidad Médica") {TMP}
     else {TMP %>% setView(lng = -98,lat = 24.3,zoom=5)}
-    })
+  })
   
-  output$caption <- renderUI({
-    if(input$nivel=="Nacional"){HTML(paste(
-  "<div class='container' style='margin-top:-40px'>",
-      "<div class='column-left' style='text-align:left;'>",
-        "<span style='font-size:50px; line-height: 50%;'>",
-          nrow(table(UM$cl_ent_clave_clCat)),
-        "</span><br>",
-          "Estados<br><br><br><br>",
-        "<span style='font-size:50px; line-height: 50%;'>",
-          prettyNum(4323,big.mark=",",scientific=FALSE),
-        "</span><br>",
-          "Usuarios<br><br><br><br>",
-         "<span style='font-size:50px; line-height: 50%;'>",
-          prettyNum(175727,big.mark=",",scientific=FALSE),
-         "</span><br>",
-            "Mensajes<br><br><br><br>",
-          "<span style='font-size:50px; line-height: 50%;'>",
-           prettyNum(20,big.mark=",",scientific=FALSE),
-          "</span><br>",
-            "Casos MIALERTA",
-        "</div>",
-  "<div class='column-center' style='text-align:left;'>",
-  "<span style='font-size:50px; line-height: 50%;'>",
-  nrow(table(UM$cl_mun_clave_clCat)),
-  "</span><br>",
-  "Municipios<br><br><br><br>",
-  "<span style='font-size:50px; line-height: 50%;'>",
-  prettyNum(3987,big.mark=",",scientific=FALSE),
-  "</span><br>",
-  "Beneficiarias<br><br><br><br>",
-  "<span style='font-size:50px; line-height: 50%;'>",
-  prettyNum(92315,big.mark=",",scientific=FALSE),
-  "</span><br>",
-  "Enviados<br><br><br><br>",
-  "<span style='font-size:50px; line-height: 50%;'>",
-  prettyNum(77,big.mark=",",scientific=FALSE),
-  "</span><br>",
-  "Cambios MICITA",
-  "</div>",
-  "<div class='column-right' style='text-align:left;'>",
-  "<span style='font-size:50px; line-height: 50%;'>",
-  nrow(UM),
-  "</span><br>",
-  "Unidades Médicas<br><br><br><br>",
-  "<span style='font-size:50px; line-height: 50%;'>",
-  prettyNum(336,big.mark=",",scientific=FALSE),
-  "</span><br>",
-  "Personal<br><br><br><br>",
-  "<span style='font-size:50px; line-height: 50%;'>",
-  prettyNum(83412,big.mark=",",scientific=FALSE),
-  "</span><br>",
-  "Recibidos<br><br><br><br>",
-  "<span style='font-size:50px; line-height: 50%;'>",
-  "8%",
-  "</span><br>",
-  "Tasa de error",
-  "</div>",
-     
-  "</div>"),sep="")} 
-    else if(input$nivel=="Estatal") {HTML(paste(
-      "<div class='container' style='margin-top:-40px'>",
-      "<div class='column-left' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      nrow(table(UM[UM$cl_ent_clave_clCat==7,]$cl_ent_clave_clCat)),
-      "</span><br>",
-      "Estados<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(1297,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Usuarios<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(7422,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Mensajes<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(3,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Casos MIALERTA",
-      "</div>",
-      "<div class='column-center' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      nrow(table(UM[UM$cl_ent_clave_clCat==7,]$cl_mun_clave_clCat)),
-      "</span><br>",
-      "Municipios<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(1237,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Beneficiarias<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(4676,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Enviados<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(21,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Cambios MICITA",
-      "</div>",
-      "<div class='column-right' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      nrow(UM[UM$cl_ent_clave_clCat==7,]),
-      "</span><br>",
-      "Unidades Médicas<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(60,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Personal<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(2746,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Recibidos<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "11%",
-      "</span><br>",
-      "Tasa de error",
-      "</div>",
-      
-      "</div>"),sep="")} 
+  output$resumen <- renderUI({
+    if(input$nivel=="Nacional"){
+      args <- list(  Estados = nrow(table(UM$cl_ent_clave_clCat)),
+                     Usuarios = prettyNum(4323,big.mark=",",scientific=FALSE),
+                     Mensajes = prettyNum(nrow(MSJ),big.mark=",",scientific=FALSE),
+                     CasosMIALERTA = prettyNum(92,big.mark=",",scientific=FALSE),
+                     Municipios = nrow(table(UM$cl_mun_clave_clCat)),
+                     Beneficiarias = prettyNum(3987,big.mark=",",scientific=FALSE),
+                     Enviados = prettyNum(nrow(MSJ[MSJ$direction=='O',]),big.mark=",",scientific=FALSE),
+                     CambiosMICITA = prettyNum( 12345,big.mark=",",scientific=FALSE),
+                     UnidadesMedicas = nrow(UM),
+                     Personal = prettyNum(336,big.mark=",",scientific=FALSE),
+                     Recibidos = prettyNum(nrow(MSJ[MSJ$direction=='I',]),big.mark=",",scientific=FALSE),
+                     Tasadeerror = "8%"
+      )
+      template_resumen(args)
+      } 
+    else if(input$nivel=="Estatal") {
+      args <- list(  Estados = nrow(table(UM[UM$cl_ent_clave_clCat==7,]$cl_ent_clave_clCat)),
+                     Usuarios = prettyNum(1297,big.mark=",",scientific=FALSE),
+                     Mensajes = prettyNum(7422,big.mark=",",scientific=FALSE),
+                     CasosMIALERTA = prettyNum(3,big.mark=",",scientific=FALSE),
+                     Municipios = nrow(table(UM[UM$cl_ent_clave_clCat==7,]$cl_mun_clave_clCat)),
+                     Beneficiarias = prettyNum(1237,big.mark=",",scientific=FALSE),
+                     Enviados = prettyNum(4676,big.mark=",",scientific=FALSE),
+                     CambiosMICITA = prettyNum(21,big.mark=",",scientific=FALSE),
+                     UnidadesMedicas = nrow(UM[UM$cl_ent_clave_clCat==7,]),
+                     Personal = prettyNum(60,big.mark=",",scientific=FALSE),
+                     Recibidos = prettyNum(2746,big.mark=",",scientific=FALSE),
+                     Tasadeerror = "11%"
+      )
+      template_resumen(args)
+      } 
     else if(input$nivel=="Unidad Médica"){"Unidad Médica"}
     else {"Nacional"}
-   })
+  })
   
   output$clinicas <- renderUI({
-    if(input$nivel=="Nacional"){HTML(paste(
-      "<div class='container' style='margin-top:10px'>",
-      "<div class='column-left' style='text-align:left;width: 50%;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      nrow(UM),
-      "</span><br>",
-      "Número de clínicas total<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum( round(nrow(UM)*.27,0),big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Número de clínicas incorporadas<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      paste(round((nrow(UM)*.27)/nrow(UM) * 100,1),"%",sep=""),
-      "</span><br>",
-      "Porcentaje de clínicas ya incorporado",
-      "</div></div>",sep=""))} 
-    else if(input$nivel=="Estatal") {HTML(paste(
-      "<div class='container' style='margin-top:10px'>",
-      "<div class='column-left' style='text-align:left;width: 50%;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      nrow(UM[UM$cl_ent_clave_clCat==7,]),
-      "</span><br>",
-      "Número de clínicas total<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum( round(nrow(UM[UM$cl_ent_clave_clCat==7,])*.27,0),big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Número de clínicas incorporadas<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      paste(round((nrow(UM[UM$cl_ent_clave_clCat==7,])*.27)/nrow(UM[UM$cl_ent_clave_clCat==7,]) * 100,1),"%",sep=""),
-      "</span><br>",
-      "Porcentaje de clínicas ya incorporado",
-      "</div></div>",sep=""))} 
+    if(input$nivel=="Nacional"){
+      arg_list <- list( 
+        Numerodeclinicastotal = nrow(UM),
+        Numerodeclinicasincorporadas = prettyNum( round(nrow(UM)*.27,0),big.mark=",",scientific=FALSE),
+        Porcentajedeclinicasyaincorporado = paste(round((nrow(UM)*.27)/nrow(UM) * 100,1),"%",sep="")
+      )
+      template_clinicas(arg_list)
+      } 
+    else if(input$nivel=="Estatal") {
+      arg_list <- list( 
+        Numerodeclinicastotal = nrow(UM[UM$cl_ent_clave_clCat==7,]),
+        Numerodeclinicasincorporadas = prettyNum( round(nrow(UM[UM$cl_ent_clave_clCat==7,])*.27,0),big.mark=",",scientific=FALSE),
+        Porcentajedeclinicasyaincorporado = paste(round((nrow(UM[UM$cl_ent_clave_clCat==7,])*.27)/nrow(UM[UM$cl_ent_clave_clCat==7,]) * 100,1),"%",sep="")
+      )
+      template_clinicas(arg_list)
+      } 
     else if(input$nivel=="Unidad Médica"){"Unidad Médica"}
     else {"Nacional"}
   })
   
   output$usuarios <- renderUI({
-    if(input$nivel=="Nacional"){HTML(paste(
-      "<div class='container' style='margin-top:10px'>",
-      "<div class='column-left' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(1235,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Usuarias totales<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(1190,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Beneficiarias Prospera<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(45,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Población abierta",
-      "</div>",
-      "<div class='column-center' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "23%",
-      "</span><br>",
-      "Con bebé (total)<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "27%",
-      "</span><br>",
-      "Con bebé (Prospera)<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "21%",
-      "</span><br>",
-      "Con bebé (Abierto)<br><br><br><br>",
-      "</div>",
-      "<div class='column-right' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "45",
-      "</span><br>",
-      "Auxiliares<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "22",
-      "</span><br>",
-      "Vocales<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "125",
-      "</span><br>",
-      "Responsables<br><br><br><br>",
-      "</div>",
-      "</div>"),sep="")} 
-    else if(input$nivel=="Estatal") {HTML(paste(
-      "<div class='container' style='margin-top:10px'>",
-      "<div class='column-left' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(423,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Usuarias totales<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(402,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Beneficiarias Prospera<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      prettyNum(21,big.mark=",",scientific=FALSE),
-      "</span><br>",
-      "Población abierta",
-      "</div>",
-      "<div class='column-center' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "31%",
-      "</span><br>",
-      "Con bebé (total)<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "28%",
-      "</span><br>",
-      "Con bebé (Prospera)<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "35%",
-      "</span><br>",
-      "Con bebé (Abierto)<br><br><br><br>",
-      "</div>",
-      "<div class='column-right' style='text-align:left;'>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "12",
-      "</span><br>",
-      "Auxiliares<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "7",
-      "</span><br>",
-      "Vocales<br><br><br><br>",
-      "<span style='font-size:50px; line-height: 50%;'>",
-      "37",
-      "</span><br>",
-      "Responsables<br><br><br><br>",
-      "</div>",
-      "</div>"),sep="")} 
+    if(input$nivel=="Nacional"){
+      args <- list(
+        Usuariastotales = prettyNum(1235,big.mark=",",scientific=FALSE),
+        BeneficiariasProspera = prettyNum(1190,big.mark=",",scientific=FALSE),
+        Poblacionabierta = prettyNum(45,big.mark=",",scientific=FALSE),
+        Conbebetotal = "23%",
+        ConbebeProspera = "27%",
+        ConbebeAbierto = "21%",
+        Auxiliares = "45",
+        Vocales = "22",
+        Responsables = "125"
+      )
+      template_usuarios(args)
+    } 
+    else if(input$nivel=="Estatal") {
+      args <- list(
+        Usuariastotales = prettyNum(423,big.mark=",",scientific=FALSE),
+        BeneficiariasProspera = prettyNum(402,big.mark=",",scientific=FALSE),
+        Poblacionabierta = prettyNum(21,big.mark=",",scientific=FALSE),
+        Conbebetotal = "31%",
+        ConbebeProspera = "28%",
+        ConbebeAbierto = "35%",
+        Auxiliares = "12",
+        Vocales = "7",
+        Responsables = "37"
+      )
+      template_usuarios(args)
+    } 
     else if(input$nivel=="Unidad Médica"){"Unidad Médica"}
     else {"Nacional"}
   })
@@ -323,9 +161,9 @@ shinyServer(function(input, output) {
   #   TimeSeries
   # })
   
-
+  
 })
 
 
-  
+
 
